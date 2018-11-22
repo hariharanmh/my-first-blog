5b84690d7ac3262f4a09623075995004a5a0b29a
hariharanmh <34367091+hariharanmh@users.noreply.github.com>
2018-11-22 19:23:45 +0530

    Update msg91.py

--- msg91.py
+++ msg91.py
@@ -2,7 +2,7 @@ import http.client
 
 def check_balance():
 	conn = http.client.HTTPConnection("control.msg91.com")
-	conn.request("GET", "/api/balance.php?authkey=184608AXJHchldS5a12eeb2&type=4")
+	conn.request("GET", "/api/balance.php?authkey=&type=4")
 	res = conn.getresponse()
 	data = res.read()
 	return data.decode("utf-8")
@@ -11,13 +11,13 @@ def check_balance():
 
 def send_SMS(mobile, message):
 	conn = http.client.HTTPConnection("api.msg91.com")
-	url  = "/api/sendhttp.php?sender=EQRATC&route=4&mobiles="+mobile+"&authkey=184608AXJHchldS5a12eeb2&country=91&message="+message
+	url  = "/api/sendhttp.php?sender=EQRATC&route=4&mobiles="+mobile+"&authkey=&country=91&message="+message
 	conn.request("GET", url)
 	res = conn.getresponse()
 	data = res.read()
 	return data.decode("utf-8")
 	
-#print(send_SMS('9488631135', 'EQRA Test Message'))
+#print(send_SMS('', ''))
 
 
 # def send_SMS_POST():
@@ -25,7 +25,7 @@ def send_SMS(mobile, message):
 # 	payload = "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Message1\", \"to\": [ \"98260XXXXX\", \"98261XXXXX\" ] }, { \"message\": \"Message2\", \"to\": [ \"98260XXXXX\", \"98261XXXXX\" ] } ] }"
 # 	headers = {
 #     'content-type': "application/json",
-#     'authkey': "184608AXJHchldS5a12eeb2"
+#     'authkey': ""
 #     }
 #     conn.request("POST", "/api/v2/sendsms", payload, headers)
 #     res = conn.getresponse()
